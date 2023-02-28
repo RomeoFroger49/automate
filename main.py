9486c8999f447b217b8f4b75fa5ed2172946bf57
RomeoFroger49 <romeo.froger@gmail.com>
2023-02-28 20:27:21 +0100

    Ajout de la conversion .JSON en dictionnaire PY et la creation des etat

--- main.py
+++ main.py
@@ -1,3 +1,54 @@
 import graphviz
 import json
 
+# Charger le contenu du fichier JSON en tant qu'objet Python
+with open('fichier.json',"r") as f:
+    data = json.load(f)
+
+# Convertir les données JSON en dictionnaire
+etape= []
+
+for etapes in data[0]:
+    etape += [etapes]
+
+dictionnaire = {}
+
+for element in data[0]:
+    dictionnaire[element] = data[0][element]
+
+print(dictionnaire)
+
+dot = graphviz.Graph("automate")
+
+etat_ecris = []
+
+#creation des etats initiaux
+for etat_init in dictionnaire[etape[2]]:
+    dot.edge(etat_init, etat_init)
+    etat_ecris += etat_init
+
+#creation des etats finaux
+for etat_final in dictionnaire[etape[3]]:
+    dot.attr('node', shape='doublecircle')
+    dot.node(etat_final)
+    etat_ecris += etat_final
+    dot.attr('node', shape='circle')
+
+nb_etat_ecris = len(etat_ecris)
+
+#creation des etats restants
+for etat_restant in dictionnaire[etape[1]]:
+    nb_etat = 0
+    for etat in etat_ecris:
+        if etat_restant != etat:
+            nb_etat += 1
+    if nb_etat == nb_etat_ecris:
+        dot.node(etat_restant)
+        nb_noeud = 0
+    else:
+        nb_noeud = 0
+
+
+
+# Affichage du graphe
+dot.view()
